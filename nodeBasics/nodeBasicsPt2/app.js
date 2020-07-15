94e9084f0e384980877a0780abdee2b1f1577096
jlabrec <jacob.labrec@gmail.com>
2020-07-15 18:22:26 -0500

    Add basic routing module

--- nodeBasics/nodeBasicsPt2/app.js
+++ nodeBasics/nodeBasicsPt2/app.js
@@ -1,8 +1,12 @@
+//built in modules
 const os = require('os');
 const http = require('http');//launch a server, send requests
 const https = require('https');//launch an SSL server
 const path = require('path');
 const fs = require('fs');
+
+//Local custom modules 
+const routes = require('./routes');
 
 
 //Three examples listed below to show different ways to create a server/listener
@@ -17,44 +21,9 @@ const fs = require('fs');
 // http.createServer(function(req,res) { 
 
 // });
-//3.
-const server = http.createServer((req,res)=>{
-    console.log(req.url,req.method,req.headers);
-    
-    if(req.url === '/'){
-        res.setHeader('Content-Type',"text/html");
-        res.write(fs.readFileSync('./html/index.html'));
-        return res.end();
-        
-    } 
-    if(req.url === '/message' && req.method === 'POST'){
-        const body = [];
-        req.on('data',chunk=> {
-            console.log(chunk);
-            body.push(chunk);
-        });
-        req.on('end',()=>{
-            const parsedBody = Buffer.concat(body).toString();
-            console.log(parsedBody);
-            const message = parsedBody.split('=')[1];
-            fs.writeFile('message.txt',message, (err)=>{
-                if(err === null) {
-                    res.writeHead(302,{location:"/"})
-                    return res.end();
-                } else {
-                    res.writeHead(500);
-                    return res.end();
-                }
-
-            });
-            //Response will be returned prior to the req.on('end') function, so if our response depends on the body, we need to move this code into that method
-            
-        });
-        
-        
-    }
-    res.end();
-});
-
+//3. The below would be allowed if routes.js just exported the single method, but since we are exporting an object, #4 is required
+//const server = http.createServer(routes)
+const server = http.createServer(routes.handler);
+console.log(routes.someText);
 server.listen(3000);
 
