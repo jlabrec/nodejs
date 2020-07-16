5b96ccc2f26bad32189fc969a1c8035e6b5eed6c
jlabrec <jacob.labrec@gmail.com>
2020-07-16 17:21:15 -0500

    Adds completed assignment 1, as well as setting up initial express application

--- expressJS/installingExpress/app.js
+++ expressJS/installingExpress/app.js
@@ -1,23 +1,32 @@
-const http = require('http');
+
+//const http = require('http'); not necessary since express is handling this so far. 
 const express = require('express');
 
 
 const app = express();
 
-app.use((req,res,next)=>{
-    console.log('In the middleware!');
-    //call next() to call the next use middleware function
+app.use('/',(req,res,next)=>{
+    console.log('this always runs');
     next();
-});
+})
 
-app.use((req,res,next)=>{
+app.use('/add-product',(req,res,next)=>{
+    res.send('<h1>Product added :100:</h1>');
+})
+
+app.use('/',(req,res,next)=>{
     console.log('This is the next');
     //Send response
-    //res.setHeader('Content-Type','application/json');
-    //res.send('{"message":"Hello from express json"}');
-    res.send('<h1>Hello from Express</h1>');
+    res.setHeader('Content-Type','application/json');
+    res.send({"message":"Hello from express json"});
+    //res.send('<h1>Hello from Express</h1>');
 
 });
-const server = http.createServer(app);
 
-server.listen(3000);
+
+
+// app.listen(3000) replaces this
+// const server = http.createServer(app);
+// server.listen(3000);
+
+app.listen(3000);
