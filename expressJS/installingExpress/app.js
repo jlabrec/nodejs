2f51b192948d9a41a0ce4fe933d14ac5a06f9028
jlabrec <jacob.labrec@gmail.com>
2020-07-17 00:50:37 -0500

    Adds body-parser dependency, and parses body from add-products submissions

--- expressJS/installingExpress/app.js
+++ expressJS/installingExpress/app.js
@@ -1,21 +1,36 @@
 
 //const http = require('http'); not necessary since express is handling this so far. 
 const express = require('express');
+const bodyParser = require('body-parser');
 
 
 const app = express();
 
+//Parse the body of the request and call next routing function, will parse form type data. 
+app.use(bodyParser.urlencoded({extended: false}));
+
+
 app.use('/',(req,res,next)=>{
-    console.log('this always runs');
+   // console.log('this always runs');
     next();
 })
 
 app.use('/add-product',(req,res,next)=>{
-    res.send('<h1>Product added :100:</h1>');
+    res.send('<form action="/product" method="POST">Product:<input type="text" name="title"><br><button type="submit">Add Product</button></form>');
 })
 
+
+
+
+app.use('/product',(req,res,next)=>{
+
+    console.log(req.body); //convenience function express provides
+    console.log(req.body.title);
+    res.redirect('/');//convenience function express provides
+});
+
 app.use('/',(req,res,next)=>{
-    console.log('This is the next');
+   // console.log('This is the next');
     //Send response
     res.setHeader('Content-Type','application/json');
     res.send({"message":"Hello from express json"});
