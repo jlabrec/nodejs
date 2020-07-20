82e027f9bfefbb804dd032d871ee6673137a988d
jlabrec <jacob.labrec@gmail.com>
2020-07-20 02:02:17 -0500

    Adds persistent model storage via json file.

--- models/product.js
+++ models/product.js
@@ -1,4 +1,19 @@
-const products = [];
+const fs = require('fs');
+const path = require('path');
+
+const rootDir = require('../util/path');
+const p = path.join(rootDir,'data','products.json');
+
+const getProductsFromFile=(callBack) => {
+    fs.readFile(p,(err,data)=>{
+        if(err) {
+            callBack([]);
+        } else{
+        callBack(JSON.parse(data).sort((a,that)=> (a.title.toLowerCase() < that.title.toLowerCase()) ? -1 : 1));
+        }
+    });
+};
+
 
 module.exports = class Product{
     constructor(title){
@@ -6,12 +21,23 @@ module.exports = class Product{
     }
 
     save() {
-        products.push(this);
-        products.sort((a,that)=>(a.title.toUpperCase()< that.title.toUpperCase())? -1:1);
+        
+        getProductsFromFile(products =>{
+            products.push(this);
+            fs.writeFile(p, JSON.stringify(products),(err)=>{
+                if(err)
+                console.log(err);
+            });
+        });
+        
+        
+        
+        
     }
 
-    static fetchAll(){
-        return products;
+    static fetchAll(callBack){
+         getProductsFromFile(callBack);
+        
     }
 
 }
