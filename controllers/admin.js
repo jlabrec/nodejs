b42be5f76972c220fc493d60666ff24e17d4d22a
jlabrec <jacob.labrec@gmail.com>
2020-07-21 19:36:15 -0500

    Adds route for editing a product, to be pre-populated later

--- controllers/admin.js
+++ controllers/admin.js
@@ -2,14 +2,12 @@ const Product = require('../models/product');
 
 exports.getAddProduct = (req,res,next)=>{
 
-    res.render('admin/add-product',{
+    res.render('admin/edit-product',{
            pageTitle:"Add Product",
-           activeAddProduct: true,
-           formsCSS:true,
-           productCSS:true,
            path: '/admin/add-product'
        });
 };
+
 
 
 exports.postAddProduct = (req,res,next)=>{
@@ -22,6 +20,16 @@ exports.postAddProduct = (req,res,next)=>{
 
 
     res.redirect('/');//convenience function express provides
+};
+
+exports.getEditProduct = (req,res,next)=>{
+    const editMode = req.query.edit;
+    if (!editMode) return res.redirect('/');
+    res.render('admin/edit-product',{
+           pageTitle:"Edit Product",
+           path: '/admin/edit-product',
+           editing: editMode
+       });
 };
 
 
--- routes/admin.js
+++ routes/admin.js
@@ -15,4 +15,6 @@ router.post('/add-product',adminController.postAddProduct);
 
 router.get('/products',adminController.getProducts);
 
+router.get('/edit-product/:productId',adminController.getEditProduct);
+
 module.exports = router; 
